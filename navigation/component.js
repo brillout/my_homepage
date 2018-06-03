7638fe682f0584507e17adbf5adfd2fb4df3976e
Joshua Simon <joshsimon@gmx.de>
2018-06-03 10:08:15 +0200

    this.props.currentPath doesnt work, try workaround

--- navigation/component.js
+++ navigation/component.js
@@ -3,24 +3,38 @@ import './style.css';
 import classnames from 'classnames';
 
 class Navigation extends React.Component {
+  constructor(props) {
+   super(props);
+    this.path = 'home';
+    this.setPath = this.setPath.bind(this);
+
+}
+
+
+  setPath(path){
+    console.log('changed path ', path);
+    this.path = path;
+  }
 
   render() {
     return <div className="navigation">
         <div className="black-bar-top">
           <p className="white-header">Joshua Simon personal website</p>
         </div>
+        <p>{ this.path }</p>
+
         <ul className="nav nav-tabs grey-background">
           <li className="nav-item">
-              <a className={classnames('nav-link', this.props.currentPath === 'home' && 'active')} href="/home.html">Home</a>
+              <a onclick={this.setPath('home')} className={classnames('nav-link', this.path === 'home' && 'active')} href="/home.html">Home</a>
           </li>
           <li className="nav-item">
-            <a className={classnames('nav-link', this.props.currentPath === 'cv' && 'active')} href="/cv.html">CV</a>
+            <a  onclick={this.setPath('cv')} className={classnames('nav-link', this.path === 'cv' && 'active')} href="/cv.html">CV</a>
           </li>
           <li className="nav-item">
-            <a className={classnames('nav-link', this.props.currentPath === 'blog' && 'active')} href="/blog.html">Blog</a>
+            <a className={classnames('nav-link', this.props.currentPath === 'blog.html' && 'active')} href="/blog.html">Blog</a>
           </li>
           <li className="nav-item">
-            <a className={classnames('nav-link', this.props.currentPath === 'recommendations' && 'active')} href="/recommendations.html">Recommendations</a>
+            <a className={classnames('nav-link', this.props.currentPath === 'recommendations.html' && 'active')} href="/recommendations.html">Recommendations</a>
           </li>
         </ul>
       </div>
