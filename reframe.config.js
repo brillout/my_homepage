e4791a3cb5a33ce3b41885c92bea17569c70ec96
Romuald Brillout <github.com@brillout.com>
2018-06-08 01:11:15 +0200

    use latest Reframe version

---
+++ reframe.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    $plugins: [
+        require('@reframe/react-kit')
+    ],
+    buildRepository: {
+        remote: 'git@github.com:JoshSimon:JoshSimon.github.io'
+    },
+};
